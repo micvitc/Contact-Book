7ba5d4a7fc387787047b0ad03fae712339305c70
Tejaswini <tejaswinithota83@gmail.com~>
2023-06-07 18:32:43 +0530

    both reqs works

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify 
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow 
+from flask import Flask, request
+
 
 app= Flask(__name__)
 marsh = Marshmallow(app)
